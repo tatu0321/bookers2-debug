d7abe877a93451b95b91d84a6d427d5913816d5d
tatu0321 <177841065+tatu0321@users.noreply.github.com>
2024-09-29 11:30:46 +0000

    [update] 応用課題5

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
     get 'followings' => 'relationships#followings', as: 'followings'
   end
+
+  get "/search" => "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
