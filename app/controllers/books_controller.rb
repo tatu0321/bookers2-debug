f194d73460df66eadf859e97422eef655a6c8e54
tatu0321 <177841065+tatu0321@users.noreply.github.com>
2024-10-02 02:24:52 +0000

    [Finish] 応用課題7a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,14 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all
+    #@books = Book.all
     @book = Book.new
+    to = Time.current.at_end_of_day
+    form = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorited_users).
+      sort_by {|x|
+        x.favorited_users.includes(:favorites).where(created_at: form...to).size
+      }.reverse
   end
 
   def create
